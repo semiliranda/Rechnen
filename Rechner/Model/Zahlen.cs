7f46144aad1e6967d0d3d950e869f5203605be37
Nikolay Shelakhaev <n.Shelakhaev@schrack.com>
2020-09-30 15:11:01 +0200

    FEATURE: Add logic

---
+++ Rechner/Model/Zahlen.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rechnen.Model
+{
+    class Zahlen
+    {
+        public decimal? Zahl1 { get; set; }
+        public decimal? Zahl2 { get; set; }
+        // so liegt das Ergebnis später noch vor
+        public decimal? _zahlErg;
+        public decimal? ZahlErg
+        {
+            get { return this._zahlErg; }
+        }
+
+        public decimal? Sub()
+        {
+            this._zahlErg = Zahl1 - Zahl2;
+            return this._zahlErg;
+        }
+
+        public decimal? Add()
+        {
+            this._zahlErg = Zahl1 + Zahl2;
+            return this._zahlErg;
+        }
+    }
+}
