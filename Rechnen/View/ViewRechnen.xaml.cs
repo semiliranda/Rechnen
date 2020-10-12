b5daa241b50a31ecac496d227dca36804ce25a3d
Nikolay Shelakhaev <n.Shelakhaev@schrack.com>
2020-10-12 13:21:14 +0200

    FEATURE: Add main elements

--- Rechnen/View/ViewRechnen.xaml.cs
+++ Rechnen/View/ViewRechnen.xaml.cs
@@ -23,5 +23,10 @@ namespace Rechnen.View
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
