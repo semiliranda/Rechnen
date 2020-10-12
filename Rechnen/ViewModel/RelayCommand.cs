49512fd3c45b8d28c1bd25b089f322be7f271850
Nikolay Shelakhaev <n.Shelakhaev@schrack.com>
2020-10-12 12:38:10 +0200

    FEATURE: Add a class that pass on functionality to other objects

---
+++ Rechnen/ViewModel/RelayCommand.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Rechnen.ViewModel
+{
+    class RelayCommand : ICommand
+    {
+        // This class is important for the variables of the interface ICommand, namely
+        // bool CanExecute(object parameter);
+        // void Execute(object parameter);
+        // event EventHandler CanExecuteChanged;
+
+        private Action<object> execute;
+        private Predicate<object> canExecute;
+        private event EventHandler CanExecuteChangedInternal;
+
+        public RelayCommand(Action<object> execute) : this(execute, DefaultCanExecute)
+        {
+
+        }
+
+        public RelayCommand(Action<object> execute, Predicate<object> canExecute)
+        {
+            if (execute == null)
+            {
+                throw new ArgumentNullException("execute");
+            }
+            if (canExecute == null)
+            {
+                throw new ArgumentNullException("canExecute");
+            }
+            this.execute = execute;
+            this.canExecute = canExecute;
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add
+            {
+                CommandManager.RequerySuggested += value;
+                this.CanExecuteChangedInternal += value;
+            }
+            remove
+            {
+                CommandManager.RequerySuggested -= value;
+                this.CanExecuteChangedInternal -= value;
+            }
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return this.canExecute != null && this.canExecute(parameter);
+        }
+
+        // here the command is being passed through
+        public void Execute(object parameter)
+        {
+            this.execute(parameter);
+        }
+
+        public void OnCanExecuteChanged()
+        {
+            EventHandler handler = this.CanExecuteChangedInternal;
+            if (handler != null)
+            {
+                handler.Invoke(this, EventArgs.Empty);
+            }
+        }
+
+        public void Destroy()
+        {
+            this.canExecute = _ => false;
+            this.execute = _ => { return; };
+        }
+
+        public static bool DefaultCanExecute(object parameter)
+        {
+            return true;
+        }
+    }
+}
