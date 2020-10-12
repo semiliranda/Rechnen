02b82a944a46a0bd6d6bde00042227557019c10a
Nikolay Shelakhaev <n.Shelakhaev@schrack.com>
2020-10-12 11:50:25 +0200

    FEATURE: Add more functionality to ViewModel

--- Rechnen/ViewModel/RechnenViewModel.cs
+++ Rechnen/ViewModel/RechnenViewModel.cs
@@ -123,5 +123,81 @@ namespace Rechnen.ViewModel
 
         // important for binding on runtime
         public ViewRechnen Formular { get; set; }
+
+        private void Addition(object obj)
+        {
+            try
+            {
+                Fehler = ""; // reset
+                // give data to model
+                this._z.Zahl1 = Convert.ToDecimal(Zahl1);
+                this._z.Zahl2 = Convert.ToDecimal(Zahl2);
+                // call method Add and convert the result into string
+                ZahlErgebins = this._z.Add().ToString();
+                Farbe();
+            } catch (Exception ex)
+            {
+                Fehler = "Fehler bei der Addition:" + Environment.NewLine + ex.Message;
+                ZahlErgebins = "Fehler!";
+            }
+        }
+
+        private void Subtraktion(object obj)
+        {
+            try
+            {
+                Fehler = ""; // reset
+                // give data to model
+                this._z.Zahl1 = Convert.ToDecimal(Zahl1);
+                this._z.Zahl2 = Convert.ToDecimal(Zahl2);
+                // call method Sub and convert the result into string
+                ZahlErgebins = this._z.Sub().ToString();
+                Farbe();
+            }
+            catch (Exception ex)
+            {
+                Fehler = "Fehler bei der Subtraktion:" + Environment.NewLine + ex.Message;
+                ZahlErgebins = "Fehler!";
+            }
+        }
+
+        private void Reset(object obj)
+        {
+            Zahl1 = "0";
+            Zahl2 = "0";
+            ZahlErgebins = "0";
+            // Color to default
+            FarbeVordergrund = Brushes.Black;
+        }
+
+        #region Hilfsmethoden
+        private void Farbe()
+        {
+            // defauld with white background
+            FarbeVordergrund = Brushes.Black;
+            if (this._z.ZahlErg > 0)
+            {
+                FarbeVordergrund = Brushes.Green;
+            }
+            if (this._z.ZahlErg < 0)
+            {
+                FarbeVordergrund = Brushes.Red;
+            }
+        }
+        #endregion
+
+        // Constructor
+        public RechnenViewModel()
+        {
+            // methods for user actions
+            _additionButtonCommand = new RelayCommand(Addition, param => this._canExecute);
+            _subtraktionButtonCommand = new RelayCommand(Subtraktion, param => this._canExecute);
+            _resetButtonCommand = new RelayCommand(Reset, param => this._canExecute);
+            // <--
+
+            _zahl1 = "0"; // set default values is (optional)
+            _zahl2 = "0";
+            _zahlErgebnis = "0";
+        }
     }
 }
